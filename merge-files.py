9b8ab5b81d314c0b3a843f90cee5088d2d1a26eb
Long-Giang Vu <giangvl.cs@gmail.com>
2025-10-04 14:58:46 -0700

    feat: update merging script

--- merge-files.py
+++ merge-files.py
@@ -1,7 +1,6 @@
 import os
 import json
 from tqdm import tqdm
-from multiprocessing import Pool, cpu_count, current_process
 
 parent_fold = '/workspace/datasetvol/mvhuman_data/face_bboxes'
 
@@ -13,7 +12,6 @@ def process_objects(args):
     """Process a list of objects and return their data."""
     obj_list, process_idx = args  # unpack
     local_data = {}
-    process_name = current_process().name
 
     for obj in tqdm(
         obj_list,
@@ -33,29 +31,33 @@ def process_objects(args):
 
     return local_data
 
+def get_pod_index():
+    """Get the pod index from environment variables."""
+    # Try different ways to get pod index
+    # hostname = os.environ.get('HOSTNAME', '')
+    return int(os.environ.get('JOB_COMPLETION_INDEX', 0))
+
 if __name__ == "__main__":
     all_objects = os.listdir(parent_fold)
     all_objects.sort()  # ensure stable order
 
     # Split into 10 roughly equal chunks
-    num_processes = 10
+    num_processes = int(os.environ.get("JOB_PARALLELISM", 1))
     chunk_size = len(all_objects) // num_processes
     chunks = [all_objects[i * chunk_size:(i + 1) * chunk_size] for i in range(num_processes - 1)]
     chunks.append(all_objects[(num_processes - 1) * chunk_size:])
 
-    # Bundle chunks with process indices (for tqdm positioning)
-    args_list = [(chunk, i) for i, chunk in enumerate(chunks)]
+    # Get pod index
+    pod_idnex = get_pod_index()
+    object_list = chunks[pod_idnex]
 
-    # Process in parallel
-    with Pool(processes=num_processes) as pool:
-        results = pool.map(process_objects, args_list)
+    result = process_objects((object_list, pod_idnex))
 
     # Save each chunk’s results separately
     output_dir = "/workspace/datasetvol/mvhuman_data/unified_face_boxes"
     os.makedirs(output_dir, exist_ok=True)
 
-    for i, res in enumerate(results):
-        out_path = os.path.join(output_dir, f"data_part_{i}.json")
-        with open(out_path, 'w') as f:
-            json.dump(res, f)
-        print(f"✅ Saved {out_path}")
+    out_path = os.path.join(output_dir, f"data_part_{pod_idnex}.json")
+    with open(out_path, 'w') as f:
+        json.dump(result, f)
+    print(f"✅ Saved {out_path}")
