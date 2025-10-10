76358509fbc33a67bbcaf901b4ce680eec8d1c5e
Long Giang Vu <giangvl.cs@gmail.com>
2025-10-10 12:42:44 -0700

    feawt: update script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-python face-recognition.py \
+python run-full-data.py \
     --root_dir /workspace/datasetvol/mvhuman_data/mv_captures \
     --output_dir /workspace/datasetvol/mvhuman_data/unified_face_boxes
